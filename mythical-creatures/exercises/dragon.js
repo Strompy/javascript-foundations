a687456150d25492f4e0416c7a2212430ddbb313
Phillip Strom <phillip.strom@gmail.com>
2021-03-17 14:50:14 -0600

    Pass dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eat_count = 0;
+  }
 
+  greet(){
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat(){
+    this.eat_count++;
+    if (this.eat_count >= 3) {
+      this.hungry = false;
+    }
+  }
+
+
+}
+
+module.exports = Dragon;
